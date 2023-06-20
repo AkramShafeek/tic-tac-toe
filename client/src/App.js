329d88b5aa6f4327ebecb4e61e1e5cc05940090e
Akram <akramshafeek70@gmail.com>
2023-06-21 00:51:09 +0530

    Frontend complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import TicTacToe from "./components/TicTacToe";
 
 function App() {
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%,-50%)'
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={style}>
+      <TicTacToe />
     </div>
   );
 }
---
+++ client/src/components/TicTacToe.jsx
@@ -0,0 +1,69 @@
+import { useEffect, useState } from "react";
+import '../styles/tictactoe.css'
+import winCheck from "../utils/winCheck";
+const TicTacToe = () => {
+  const matrix = [["", "", ""], ["", "", ""], ["", "", ""]];
+  const [gameMatrix, setGameMatrix] = useState(matrix);
+  const [chance, setChance] = useState('X');
+  const [isGameOver, setIsGameOver] = useState(false);
+  const [winner, setWinner] = useState('');
+
+  useEffect(() => {
+    const status = (winCheck(gameMatrix));
+    if (status !== 'continue') {
+      setIsGameOver(true);
+      setWinner(status);
+    }
+
+  }, [gameMatrix]);
+
+  const updateGameMatrix = (rowIndex, colIndex) => {
+    if (gameMatrix[rowIndex][colIndex])
+      return;
+    if (isGameOver)
+      return;
+    const newMatrix = [];
+    gameMatrix.forEach((row) => {
+      newMatrix.push([...row]);
+    })
+    newMatrix[rowIndex][colIndex] = chance;
+
+    setChance(chance === "X" ? "O" : "X")
+    setGameMatrix(newMatrix);
+  }
+
+  const reset = () => {
+    setIsGameOver(false);
+    setGameMatrix(matrix);
+    setChance('X');
+    setWinner('');
+  }
+
+  return (
+    <div id="tictactoe">
+      <div className="status">
+        {!isGameOver && <p>{`${chance}'s chance`}</p>}
+        {isGameOver && (winner === 'draw' ? <p>Game is a draw</p> : <p>{`${winner} wins`}</p>)}
+      </div>
+      <div id="matrix">
+        {gameMatrix.map((row, rowIndex) => {
+          return (
+            <div key={rowIndex} className="row">
+              {row.map((cell, colIndex) => {
+                return <div
+                  key={colIndex}
+                  className="cell"
+                  onClick={() => updateGameMatrix(rowIndex, colIndex)}>
+                  {cell}
+                </div>
+              })}
+            </div>
+          )
+        })}
+      </div>
+      <button type="button" onClick={reset} style={{ visibility: isGameOver ? 'visible' : 'hidden' }}>RESET</button>
+    </div>
+  )
+}
+
+export default TicTacToe;
